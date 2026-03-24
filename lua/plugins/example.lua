c572aa65fd1dcddb08e1f8a6fd1b54998699c7fd
Yisusmgzzz <jesusestebamm@gmail.com>
2026-03-23 22:52:55 -0500

    changes

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -110,7 +110,13 @@ return {
       },
     },
   },
-
+  {
+    "lopi-py/luau-lsp.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
   { import = "lazyvim.plugins.extras.lang.typescript" },
