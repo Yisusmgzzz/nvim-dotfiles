4fbcc20cc0ca60cef72770a1666a17ecbfe99cbd
Yisusmgzzz <jesusestebamm@gmail.com>
2026-03-23 22:45:22 -0500

    cambio de animacion

--- lua/plugins/smear.lua
+++ lua/plugins/smear.lua
@@ -1,4 +1,9 @@
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  opts = {
+    cursor_color = "#ff00ff",
+    particles_enabled = true,
+    stiffness = 0.8,
+    trailing_exponent = 10,
+  },
 }
